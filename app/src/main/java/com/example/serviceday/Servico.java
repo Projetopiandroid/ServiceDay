a22c245b4fde8cfd12d325cd3669e21c39b469f2
Junior portela <Projetopi04@gmail.com>
2019-05-27 21:10:09 -0300

    modificações nas telas de serviços

--- app/src/main/java/com/example/serviceday/Servico.java
+++ app/src/main/java/com/example/serviceday/Servico.java
@@ -7,7 +7,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 public class Servico extends Fragment {
@@ -31,7 +33,20 @@ public class Servico extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_servico, container, false);
 
+        EditText procura = v.findViewById(R.id.editText8);
+        Button pesquisa = v.findViewById(R.id.button4);
+        ListView lista = v.findViewById(R.id.listaid);
 
+
+        //Ação do Botao--------------------------------------------------------------------------
+        View.OnClickListener ver = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        pesquisa.setOnClickListener(ver);
+        //----------------------------------------------------------------------------------------
 
         return v;
         }
--- app/src/main/java/com/example/serviceday/ServicoCadastro.java
+++ app/src/main/java/com/example/serviceday/ServicoCadastro.java
@@ -7,7 +7,10 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 public class ServicoCadastro extends Fragment {
@@ -30,6 +33,35 @@ public class ServicoCadastro extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_servico_cadastro, container, false);
+
+        EditText descricao = v.findViewById(R.id.editText12);
+        EditText servico = v.findViewById(R.id.editText9);
+        EditText bairro = v.findViewById(R.id.editText11);
+        Spinner spinner = v.findViewById(R.id.spinnerid);
+        Button Salvar = v.findViewById(R.id.button3);
+
+
+        //Criando o Spiner e Populando valores
+        String [] valores ={"Marcenaria","Carpintaria","Energia","outros"};
+
+        ArrayAdapter<String> LTRadapter = new ArrayAdapter<String>(this.getActivity(),android.R.layout.simple_spinner_item, valores);
+        LTRadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(LTRadapter);
+        //-----------------------------------------------------------------------------------------------
+
+        //Ação de Salvar no Banco
+        View.OnClickListener enviar = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        };
+
+        Salvar.setOnClickListener(enviar);
+        //--------------------------------------------------------------------------------
+
 
 
 
